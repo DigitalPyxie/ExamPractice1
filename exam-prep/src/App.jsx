cae300523ef8761915daca3091531a599d057816
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2025-12-03 23:44:18 +0000

    more stuff done

--- exam-prep/src/App.jsx
+++ exam-prep/src/App.jsx
@@ -3,15 +3,16 @@ import { createTrailbaseProvider } from './ra-trailbase.js';
 
 
 const Trailbase_URL = "https://animated-couscous-x45j7jx7x6w3grv-4000.app.github.dev/";
-const {dataProvider, authProvider} = createTrailbaseProvider(Trailbase_URL);
+const {dataProvider, authProvider} = await createTrailbaseProvider(Trailbase_URL);
 
 //Import each page
+import { ListPayments } from './resources/payments/ListPayments.jsx';
 
 
 //Resources
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
-   <Resource name="users" list={ListGuesser} show={ShowGuesser}/>
+   <Resource name="ListPayments" list={ListPayments} show={ShowPayments}/>
    <Resource name="users" list={ListGuesser} />
   </Admin>
 ); 
