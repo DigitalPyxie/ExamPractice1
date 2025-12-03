5fbc976e60e74faa61d03cc2b7cae5b41a0c6b3e
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2025-12-03 22:39:33 +0000

    created files

--- exam-prep/src/App.jsx
+++ exam-prep/src/App.jsx
@@ -2,7 +2,7 @@ import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-ad
 import { createTrailbaseProvider } from './ra-trailbase.js';
 
 
-const Trailbase_URL = "https://animated-couscous-x45j7jx7x6w3grv-4000.app.github.dev";
+const Trailbase_URL = "https://animated-couscous-x45j7jx7x6w3grv-4000.app.github.dev/";
 const {dataProvider, authProvider} = createTrailbaseProvider(Trailbase_URL);
 
 //Import each page
@@ -14,4 +14,4 @@ export const App = () => (
    <Resource name="users" list={ListGuesser} show={ShowGuesser}/>
    <Resource name="users" list={ListGuesser} />
   </Admin>
-);
+); 
