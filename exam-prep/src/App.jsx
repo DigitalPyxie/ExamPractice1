0924fd3f57e64a2b71d7986271f5bc477ade803c
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2025-12-04 00:24:53 +0000

    moved await after imports

--- exam-prep/src/App.jsx
+++ exam-prep/src/App.jsx
@@ -2,11 +2,11 @@ import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-ad
 import { createTrailbaseProvider } from './ra-trailbase.js';
 
 
-const Trailbase_URL = "https://animated-couscous-x45j7jx7x6w3grv-4000.app.github.dev/";
-const {dataProvider, authProvider} = await createTrailbaseProvider(Trailbase_URL);
-
 //Import each page
 import { ListPayments } from './financial/resources/payments/ListPayments.jsx';
+
+const Trailbase_URL = "https://animated-couscous-x45j7jx7x6w3grv-4000.app.github.dev/";
+const {dataProvider, authProvider} = await createTrailbaseProvider(Trailbase_URL);
 
 
 //Resources
